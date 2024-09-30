5236b57668d474cd6aa9df61c23fdb329c2fbede
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2024-09-30 18:10:24 +0100

    WIP

---
+++ tests/Requests/Projects/FetchAListOfPaymentTypesRequestTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use CodebarAg\Bexio\BexioConnector;
+use CodebarAg\Bexio\Requests\PaymentTypes\FetchAListOfPaymentTypesRequest;
+use CodebarAg\Bexio\Requests\Projects\FetchAListOfProjectsRequest;
+use Illuminate\Support\Collection;
+use Saloon\Http\Faking\MockResponse;
+use Saloon\Laravel\Http\Faking\MockClient;
+
+it('can perform the request', closure: function () {
+    $mockClient = new MockClient([
+        FetchAListOfProjectsRequest::class => MockResponse::fixture('Projects/fetch-a-list-of-projects'),
+    ]);
+
+    $connector = new BexioConnector;
+    $connector->withMockClient($mockClient);
+
+    $response = $connector->send(new FetchAListOfProjectsRequest);
+
+    $mockClient->assertSent(FetchAListOfProjectsRequest::class);
+
+    expect($response->dto())->toBeInstanceOf(Collection::class)
+        ->and($response->dto()->count())->toBe(1);
+});
