ab82179b7b5a45bc6724331f296bc967eb5bc157
Sebastian Fix <sebastian.fix@codebar.ch>
2025-06-13 11:47:24 +0200

    wip

--- config/bexio.php
+++ config/bexio.php
@@ -3,15 +3,22 @@
 return [
     'auth' => [
         'use_oauth2' => env('BEXIO_USE_OAUTH2', false),
-
         'token' => env('BEXIO_API_TOKEN'),
-
-        'oauth2' => [
-            'client_id' => env('BEXIO_OAUTH2_CLIENT_ID'),
-            'client_secret' => env('BEXIO_OAUTH2_CLIENT_SECRET'),
-            'email' => env('BEXIO_OAUTH2_EMAIL'),
-            'scopes' => [],
-        ],
+        'client_id' => env('BEXIO_OAUTH2_CLIENT_ID'),
+        'client_secret' => env('BEXIO_OAUTH2_CLIENT_SECRET'),
+        'oauth_email' => env('BEXIO_OAUTH2_EMAIL'),
+        'scopes' => [],
     ],
+
+    /*	'auth' => [
+            'token' => env('BEXIO_API_TOKEN'),
+
+            'oauth2' => [
+                'client_id' => env('BEXIO_OAUTH2_CLIENT_ID'),
+                'client_secret' => env('BEXIO_OAUTH2_CLIENT_SECRET'),
+                'email' => env('BEXIO_OAUTH2_EMAIL'),
+                'scopes' => [],
+            ],*/
+
     'route_prefix' => 'bexio',
 ];
