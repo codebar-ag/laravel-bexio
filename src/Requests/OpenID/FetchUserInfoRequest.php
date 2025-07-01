96fa77c688094befe33f6a3d0cf1e1f697c9d6e6
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2025-07-02 00:28:16 +0100

    WIP

---
+++ src/Requests/OpenID/FetchUserInfoRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CodebarAg\Bexio\Requests\OpenID;
+
+use CodebarAg\Bexio\Dto\OAuthConfiguration\OpenIDConfigurationDTO;
+use CodebarAg\Bexio\Dto\OpenID\UserInfoDTO;
+use CodebarAg\Bexio\Requests\OAuth\OpenIDConfigurationRequest;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+
+class FetchUserInfoRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function resolveEndpoint(): string
+    {
+        /** @var OpenIDConfigurationDTO $openIDConfiguration */
+        $openIDConfiguration = (new OpenIDConfigurationRequest)->send()->dto();
+
+        return $openIDConfiguration->userinfoEndpoint;
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            'Accept' => 'application/json',
+        ];
+    }
+
+    public function createDtoFromResponse(Response $response): UserInfoDTO
+    {
+        if (!$response->successful()) {
+            throw new \Exception('Request was not successful. Unable to create DTO.');
+        }
+
+        return UserInfoDTO::fromResponse($response);
+    }
+}
