b140790344b3c504cda2875cf718e7ae9118bd25
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2025-07-02 10:23:29 +0100

    WIP

--- tests/Requests/OAuth/EndSessionRequestTest.php
+++ tests/Requests/OAuth/EndSessionRequestTest.php
@@ -1,11 +1,13 @@
 <?php
 
 use CodebarAg\Bexio\Requests\OAuth\EndSessionRequest;
+use CodebarAg\Bexio\Requests\OAuth\OpenIDConfigurationRequest;
 use Saloon\Http\Faking\MockResponse;
 use Saloon\Laravel\Saloon;
 
 it('can perform the request', closure: function () {
     Saloon::fake([
+        OpenIDConfigurationRequest::class => MockResponse::fixture('OAuth/openid-configuration'),
         EndSessionRequest::class => MockResponse::fixture('OAuth/end-session'),
     ]);
 
